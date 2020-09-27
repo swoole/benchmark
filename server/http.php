5cce03c8bb7c440ec14383953ad37a7fe2d11772
matyhtf <mikan.tenny@gmail.com>
2020-09-27 19:10:33 +0800

    Optimize table test

--- server/http.php
+++ server/http.php
@@ -9,7 +9,7 @@ use Swoole\Http\Request;
 //$http = new Server("127.0.0.1", 9501);
 $http = new Server("127.0.0.1", 9502, SWOOLE_BASE);
 
-$pool = [];
+$pool = new SplQueue();
 
 $http->set(
     [
@@ -21,7 +21,7 @@ $http->set(
 
 $http->on(
     'request',
-    function (Request $request, Response $response) use (&$pool) {
+    function (Request $request, Response $response) use (&$pool, $http) {
 //    var_dump($request->server['request_uri']);
         if ($request->server['request_uri'] == '/') {
             $response->header('Last-Modified', 'Thu, 18 Jun 2015 10:24:27 GMT');
@@ -29,14 +29,18 @@ $http->on(
             $response->header('Accept-Ranges', 'bytes');
             $response->end(SwooleBench\get_response($request->getContent()));
         } elseif ($request->server['request_uri'] == '/redis') {
-            $redis = new redis;
-            $redis->connect('127.0.0.1', 6379);
+            if (count($pool) > 0) {
+                $redis = $pool->pop();
+            }
+            else {
+                $redis = new redis;
+                $redis->connect('127.0.0.1', 6379);
+            }
             $value = $redis->get('key');
-            $redis->close();
-            $pool[] = $redis;
+            $pool->push($redis);
             $response->end("<h1>Value=" . $value . "</h1>");
-        } elseif ($request->server['request_uri'] == '/redis') {
-            $response->end("<pre>" . var_export($pool, 1) . "</pre>\n");
+        } elseif ($request->server['request_uri'] == '/redis_pool_status') {
+            $response->end("<pre> Worker#".$http->getWorkerId().', PoolSize=' . var_export(count($pool), 1) . "</pre>\n");
         }
     }
 );
--- table.php
+++ table.php
@@ -395,3 +395,76 @@ function table_delete_and_incr()
         }
     }
 }
+
+function table_parallel_delete()
+{
+    $table = new Swoole\Table(131072);
+    $table->column('col0', Swoole\Table::TYPE_INT, 4);
+    $table->column('col1', Swoole\Table::TYPE_INT, 4);
+    $table->column('col100', Swoole\Table::TYPE_INT, 8);
+    $table->column('col2', Swoole\Table::TYPE_STRING, 32);
+    $table->column('col3', Swoole\Table::TYPE_STRING, 32);
+    $table->column('col4', Swoole\Table::TYPE_STRING, 4);
+    $table->column('col5', Swoole\Table::TYPE_STRING, 24);
+    $table->column('col6', Swoole\Table::TYPE_STRING, 255);
+    $table->column('col7', Swoole\Table::TYPE_INT, 1);
+    $table->column('col8', Swoole\Table::TYPE_INT, 4);
+    $table->column('col9', Swoole\Table::TYPE_INT, 4);
+    $table->column('col10', Swoole\Table::TYPE_INT, 4);
+    $table->column('col11', Swoole\Table::TYPE_INT, 4);
+    $table->column('col12', Swoole\Table::TYPE_INT, 4);
+    $table->column('col13', Swoole\Table::TYPE_INT, 4);
+    $table->create();
+
+    echo "测试开始\n";
+    echo "模拟插入65535个记录\n";
+    $s = microtime(true);
+    $NUM = 65536;
+    //模拟插入65535个记录
+    for ($i = 1; $i <= $NUM; $i++) {
+        $data = array(
+            'col0' => $i,
+            'col1' => $i - 1,
+            'col100' => mt_rand(10000000000, 19999999999),
+            'col2' => md5("asfsadfasdfsda"),
+            'col3' => md5("asfsadfasdfsda"),
+            'col4' => 'ew45',
+            'col5' => 'wahaha',
+            'col6' => 'asdjasdasdasdasdsfdfsdfsdfdsfjlskdfjldkjfasjdhaskjdhakdjfhksjdfhkjsdhfksjdfhjksdfhskfhskjdfhksdjhfkjsdhffsdddsjdhfkjsdhlksdhjlsdkjfh',
+            'col7' => 1,
+            'col8' => 11231231232,
+            'col9' => 15123123123,
+            'col10' => 2352342342,
+            'col11' => 14232,
+            'col12' => 13123123,
+            'col13' => 30,
+        );
+        $table->set("u" . $i, $data);
+    }
+    echo "插入完成, 用时：" . (microtime(true) - $s) . "\n";
+    echo "实际共享内存: " . $table->count() . "个key\n";
+
+    $workerNum = C;
+    while ($workerNum--) {
+        (new Swoole\Process(function () use ($table, $workerNum, $NUM) {
+            //【请手动调整这个值，从小到大】
+            // 从共享内存中删除的key的个数，保证每个worker都删除同样的key，
+            // 当这个值日从小增大到5000+ 以后，删除key的结果变得越来越不稳定，大于65000后，结果很诡异
+            $num_to_del = rand($NUM / 2, $NUM * 2);
+            $s = microtime(true);
+            for ($i = 1; $i <= $num_to_del; $i++) {
+                $table->del("u" . $i);
+            }
+            $expectNum = $num_to_del > $NUM ? 0 : $NUM - $num_to_del;
+            echo "worker " . $workerNum . " 从共享内存删除" . $num_to_del . "个key后， 期望剩余${expectNum}实际共享内存还剩"
+                . $table->count() . "个key, 用时：" . (microtime(true) - $s) . "\n";
+        }))->start();
+    }
+
+    $workerNum = C;
+    while ($workerNum--) {
+        Swoole\Process::wait();
+    }
+    echo "实际共享内存: " . $table->count() . "个key\n";
+    $table->destroy();
+}
